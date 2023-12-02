5b06b5277f0e4cc7a84a97beba50cde3139f913b
Mateo Alberti <mateo.alberti@gmail.com>
2023-12-02 18:54:42 -0300

    Matrix, Moviegoer, resistor and twoFer

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -5,6 +5,10 @@ class SimpleCalculator
   end
 
   def self.calculate(first_operand, second_operand, operation)
+    # checking argument is previous to the operation
+    unless first_operand.is_a?(Numeric) && second_operand.is_a?(Numeric)
+      raise ArgumentError.new("ArgumentError")
+    end
     case operation
     when '+'
       return "#{first_operand} #{operation} #{second_operand} = #{first_operand + second_operand}"
@@ -12,11 +16,10 @@ class SimpleCalculator
       return "#{first_operand} #{operation} #{second_operand} = #{first_operand * second_operand}"
     when '/'
       begin
-        second_operand == 0
-      rescue
-        puts "Division by zero is not allowed."
+        return "#{first_operand} #{operation} #{second_operand} = #{first_operand / second_operand}"
+      rescue ZeroDivisionError
+        return "Division by zero is not allowed."
       end
-      return "#{first_operand} #{operation} #{second_operand} = #{first_operand / second_operand}"
     else 
       raise UnsupportedOperation.new('UnsupportedOperation')
     end
